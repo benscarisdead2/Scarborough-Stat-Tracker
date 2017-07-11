734dc923fbbf6cfc5f87781b735b76492ef0a23c
ben scarborough <benscarborough@scarflix.com>
2017-07-10 22:18:53 -0400

    Only so far.

--- routes/index.js
+++ routes/index.js
@@ -6,102 +6,90 @@ const Schema = mongoose.Schema;
 mongoose.Promise = require("bluebird");
 mongoose.connect("mongodb://localhost:27017/activity");
 
-const activitySchema = new Schema({
-    actDesc: { type: String, required: true, unique: true }
-});
-
 const statSchema = new Schema({
-    statNumber: { type: Number },
-    statDate: { type: Date, default: Date.now },
-    activityId: { type: Schema.Types.ObjectId, ref: 'actModel' }
+  statistic: {
+    type: Number
+  },
+  statDate: {
+    Date: {type: Date, default: Date.now()},
+  }
 });
 
-const actModel = mongoose.model('activities', activitySchema);
-const statModel = mongoose.model('stats', statSchema);
+const activitySchema = new Schema({
+  activityName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  activityStats : [statSchema]
+});
 
-// actModel.insertMany([
-//     {actDesc: "swimming"},
-//     {actDesc: "running"},
-//     {actDesc: "basketball"},
-//     {actDesc: "driving"},
-//     {actDesc: "walking"},
-//     {actDesc: "skydiving"}
-// ])
+const actModel = mongoose.model('activities', activitySchema)
 
 //
 //  GET routers
 //
 
-router.get('/', function (req, res) {
-    actModel.find().then(function (items) {
-        res.render('index', { activity: items });
-    })
+router.get('/', function(req, res) {
+  actModel.find().then(function(items) {
+    res.render('index', {
+      activity: items
+    });
+  })
 })
 
-router.get('/activities', function (req, res) {
-    // all the activities with all their stats
-    var output = [];
-    actModel.find({}).then(function (allActivities) {
-        if (allActivities) {
-            allActivities.forEach(function (elt) {
-                statModel.find({activityId: elt._id}).then(function(stats){
-                    output.push(stats) 
-                      
-                })
-            })
-            console.log(output)
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(output);  
-        }
-    })
+router.get('/activities', function(req, res) {
+  // all the activities with all their stats
+  actModel.find({}).then(function(allActivities) {
+    if (allActivities) {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200).json(allActivities)
+    }
+  })
 })
 
 
-router.get('/activities/:description', function (req, res) {
-    let description = req.params.description.toLowerCase();
-    actModel.find({ actDesc: description }).then(function (activity) {
-        if (activity) {
-            statModel.find({ activityId: activity._id }).then(function (stats) {
-                if (stats) {
-                    res.setHeader('Content-Type', 'application/json')
-                    res.status(200).json(stats)
-                }
-            })
-        }
-    })
+router.get('/activities/:description', function(req, res) {
+  let description = req.params.description.toLowerCase();
+  actModel.find({activityName: description}).then(function(activity) {
+    if (activity) {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200).json(activity)
+    }
+  })
 })
 
 //
 //  POSTS
 //
 
-router.post('/activities/:description/:stat', function (req, res) {
-    let description = req.params.description.toLowerCase();
-    // find the _id for the given activity
-    actModel.findOne({ actDesc: description }).then(function (item) {
-        if (item) {
-            // use the _id to store the stat
-            statModel.create({ activityId: item._id, statNumber: req.params.stat }).then(function (stats) {
-                if (stats) {
-                    res.setHeader('Content-Type', 'application/json')
-                    res.status(200).json(stats)
-                }
-            })
-        }
-    })
+router.post('/activities/:description/stats/:statistic', function(req, res) {
+  // find the _id for the given activity
+  actModel.findOne({
+    activityName: req.params.description.toLowerCase()
+  }).then(function(selectedActivity) {
+    if (selectedActivity) {
+      // use the _id to store the stat
+      selectedActivity.activityStats.push({ statistic: req.params.statistic })
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200).json(selectedActivity)
+    }
+  })
 
 })
 
-router.post('/activities/:description', function (req, res) {
-    let description = req.params.description.toLowerCase();
-    actModel.create({ actDesc: description }).then(function (items) {
-        if (items) {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(items)
-        } else {
-            res.status(400).send("Activity already exists. Please try again.")
-        }
-    })
+router.post('/activities/:description', function(req, res) {
+  let description = req.params.description.toLowerCase();
+  actModel.create({
+    activityName: description
+  }).then(function(items) {
+    if (items) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(items)
+    } else {
+      res.status(400).send("Activity already exists. Please try again.")
+    }
+  })
 })
 
 
@@ -109,29 +97,35 @@ router.post('/activities/:description', function (req, res) {
 //  PUTS
 //
 
-router.put('/activities/:description/:newDescription', function (req, res) {
-    let description = req.params.description.toLowerCase();
-    let newDescription = req.params.newDescription.toLowerCase();
-    actModel.updateOne({ actDesc: description }, { actDesc: newDescription }).then(function (items) {
-        if (items) {
-            res.setHeader('Content-Type', 'application/json')
-            res.status(200).json(items)
-        }
-    })
+router.put('/activities/:description/:newDescription', function(req, res) {
+  let description = req.params.description.toLowerCase();
+  let newDescription = req.params.newDescription.toLowerCase();
+  actModel.updateOne({
+    activityName: description
+  }, {
+    activityName: newDescription
+  }).then(function(items) {
+    if (items) {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200).json(items)
+    }
+  })
 })
 
 //
 // DELETES
 //
 
-router.delete('/activities/:description', function (req, res) {
-    let description = req.params.description.toLowerCase();
-    actModel.deleteOne({ actDesc: description }).then(function (items) {
-        if (items) {
-            res.setHeader('Content-Type', 'application/json')
-            res.status(200).json(items)
-        }
-    })
+router.delete('/activities/:description', function(req, res) {
+  let description = req.params.description.toLowerCase();
+  actModel.deleteOne({
+    activityName: description
+  }).then(function(items) {
+    if (items) {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200).json(items)
+    }
+  })
 });
 
 
